8c97738a717366076ba5e56d706ec15c5fb30216
Artyom Nalyotov <whalewander@yandex.ru>
2025-10-16 12:42:49 +0200

    add user input receiver

--- labyrinth_game/player_actions.py
+++ labyrinth_game/player_actions.py
@@ -3,3 +3,10 @@ def show_inventory(game_state):
         print(f"Инвентарь: {game_state['player_inventory']}")
     else:
         print("Ваш инвентарь пуст.")
+
+def get_input(prompt="> "):
+    try:
+        return input(prompt)
+    except (KeyboardInterrupt, EOFError):
+        print("\nВыход из игры.")
+        return "quit"
