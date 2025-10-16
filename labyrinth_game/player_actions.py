5b4a5bc0b5bbc527838e140c790f9d84993d18bd
Artyom Nalyotov <whalewander@yandex.ru>
2025-10-16 12:28:26 +0200

    add show_inventory function

---
+++ labyrinth_game/player_actions.py
@@ -0,0 +1,5 @@
+def show_inventory(game_state):
+    if game_state['player_inventory'] != []:
+        print(f"Инвентарь: {game_state['player_inventory']}")
+    else:
+        print("Ваш инвентарь пуст.")
