76db9b005c4521d72f4775cb9d2ee18c2d266632
Artyom Nalyotov <whalewander@yandex.ru>
2025-10-21 11:53:42 +0200

    add process_command function (works on interaction with the world)

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -10,12 +10,35 @@ game_state = {
     'steps_taken': 0 # Количество сделанных шагов
 }
 
+def process_command(game_state, command):
+    if not isinstance(command, str):
+        print("Пожалуйста, введите строку, чтобы начать.")
+    
+    if ' ' in command:
+        components = command.split()
+        match components[0]:
+            case 'take':
+                player_actions.take_item(game_state, components[1])
+            case 'use':
+                player_actions.use_item(game_state, components[1])
+            case 'go':
+                player_actions.move_player(game_state, components[1])
+    else:
+        match command:
+            case 'look':
+                utils.describe_current_room(game_state)
+            case 'inventory':
+                player_actions.show_inventory(game_state)
+            case 'quit':
+                exit()
+
 def main():
     print("Добро пожаловать в Лабиринт сокровищ!\n")
     utils.describe_current_room(game_state)
     
     while not game_state['game_over']:
         command = player_actions.get_input()
+        process_command(game_state, command)
 
 
 if __name__ == "__main__":
