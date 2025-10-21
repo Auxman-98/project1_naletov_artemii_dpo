f0da5f1dfe76f75e03a9606d96010568711983fb
Artyom Nalyotov <whalewander@yandex.ru>
2025-10-21 11:25:48 +0200

    add use_item function

--- labyrinth_game/player_actions.py
+++ labyrinth_game/player_actions.py
@@ -37,3 +37,18 @@ def take_item(game_state, item):
         print("Вы подняли:", item)
     else:
         print("Такого предмета здесь нет.")
+def use_item(game_state, item_name):
+    if item_name in game_state['player_inventory']:
+        match item_name:
+            case 'torch':
+                print("Здесь стало светлее..")
+            case 'sword':
+                print("Чувствуете себя увереннее перед неизвестным.")
+            case 'bronze box':
+                print("Бронзовая шкатулка открыта.")
+                if 'rusty key' not in game_state['player_inventory']:
+                    game_state['player_inventory'].append('rusty key')
+                else:
+                    print("Здесь пусто.")
+            case _:
+                print(f"Вы не знаете, как использовать {item_name}")
